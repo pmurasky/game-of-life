7f16fb83cc05f4ddc23223171442c2db40240a8e
Peter Murasky <pmurasky@ford.com>
2019-05-03 09:19:51 -0400

    can create 3x3 board with initial state

--- src/test/java/murasky/gameoflife/rule/board/BoardCreatorTest.java
+++ src/test/java/murasky/gameoflife/rule/board/BoardCreatorTest.java
@@ -21,4 +21,19 @@ public class BoardCreatorTest {
 
         assertThat(actualBoard).isEqualToComparingFieldByField(expectedBoard);
     }
+
+    @Test
+    public void canCreateBoard3x3WithState() {
+        BoardCreator boardCreator = new BoardCreator();
+        CellState[][] expectedCells = {{ALIVE, DEAD, DEAD},
+                                       {DEAD, ALIVE, ALIVE},
+                                       {DEAD, DEAD, ALIVE} };
+        Board expectedBoard = new Board(expectedCells);
+
+        Board actualBoard = boardCreator.create(ALIVE, DEAD, DEAD,
+                                                DEAD, ALIVE, ALIVE,
+                                                DEAD, DEAD, ALIVE);
+
+        assertThat(actualBoard).isEqualToComparingFieldByField(expectedBoard);
+    }
 }
