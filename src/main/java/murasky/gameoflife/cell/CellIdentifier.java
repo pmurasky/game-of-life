75c50cbbe15584b61fc92bad9bf376ce47ac75de
Peter Murasky <pmurasky@ford.com>
2019-05-03 14:20:04 -0400

    can find neighbors for top left cell and middle cell

---
+++ src/main/java/murasky/gameoflife/cell/CellIdentifier.java
@@ -0,0 +1,26 @@
+package murasky.gameoflife.cell;
+
+import murasky.gameoflife.board.Coordinate;
+import murasky.gameoflife.board.OffsetCoordinate;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CellIdentifier {
+    public List<OffsetCoordinate> findNeighborsOffsetCoordinates(Coordinate coordinate, int lengthOfSide) {
+
+        if (coordinate.getX() == 0 && coordinate.getY() == 0){
+            return Arrays.asList(new OffsetCoordinate(0,1),
+                    new OffsetCoordinate(1,0), new OffsetCoordinate(1,1));
+        }
+        if (coordinate.getX() != 0 && coordinate.getY() != 0){
+            return Arrays.asList(new OffsetCoordinate(-1,-1),
+                    new OffsetCoordinate(-1,0), new OffsetCoordinate(-1,1),
+                    new OffsetCoordinate(0,-1), new OffsetCoordinate(0,1), new OffsetCoordinate(1,-1),
+                    new OffsetCoordinate(1,0), new OffsetCoordinate(1,1));
+        }
+
+
+        return null;
+    }
+}
